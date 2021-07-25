b5405faa2655520b74109f0eef70bdd3e2cadb70
Tulio Faria <tuliofaria@gmail.com>
2021-07-25 13:09:34 -0300

    moving from mocha to jest

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,15 +1,15 @@
-const home = (req, res) => res.send('CI/CD Project - dev - jenkins')
+const home = (req, res) => res.send('CI/CD Project')
 const page1 = (req, res) => res.send('Page1')
 const page2 = (req, res) => res.send('Page2')
 const calc = (req, res) => {
   const num1 = parseInt(req.params.num1)
   const num2 = parseInt(req.params.num2)
-  if(num2 === 0){
+  if (num2 === 0) {
     res.send('DivisaoZero')
-  }else{
-    if(num1%num2===0){
+  } else {
+    if (num1 % num2 === 0) {
       res.send('Divisivel')
-    }else{
+    } else {
       res.send('NaoDivisivel')
     }
   }
